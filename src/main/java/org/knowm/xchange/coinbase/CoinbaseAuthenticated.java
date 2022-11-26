e1172bb1197a9443dda3467b14f9e1659e78e976
김현욱 <company@wiselight.kr>
2022-11-26 21:39:02 +0900

    fix: 요청 헤더 상수 누락 해결

--- src/main/java/org/knowm/xchange/coinbase/CoinbaseAuthenticated.java
+++ src/main/java/org/knowm/xchange/coinbase/CoinbaseAuthenticated.java
@@ -5,4 +5,10 @@ import javax.ws.rs.core.MediaType;
 
 @Produces(MediaType.APPLICATION_JSON)
 public interface CoinbaseAuthenticated extends Coinbase {
+
+    String CB_ACCESS_KEY = "CB-ACCESS-KEY";
+    String CB_ACCESS_SIGN = "CB-ACCESS-SIGN";
+    String CB_ACCESS_TIMESTAMP = "CB-ACCESS-TIMESTAMP";
+    String CB_ACCESS_PASSPHRASE = "CB-ACCESS-PASSPHRASE";
+
 }
