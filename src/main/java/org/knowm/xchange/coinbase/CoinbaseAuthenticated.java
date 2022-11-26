ea0934c777e0ea5314ca27f54c428f6f3e7f58aa
김현욱 <company@wiselight.kr>
2022-11-26 21:25:15 +0900

    feat: Coinbase API Interface 생성

---
+++ src/main/java/org/knowm/xchange/coinbase/CoinbaseAuthenticated.java
@@ -0,0 +1,8 @@
+package org.knowm.xchange.coinbase;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Produces(MediaType.APPLICATION_JSON)
+public interface CoinbaseAuthenticated extends Coinbase {
+}
